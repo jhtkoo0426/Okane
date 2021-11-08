ad8125395091b34a0b399a9f5839f4da9660257b
jhtkoo0426 <jhtkoo@gmail.com>
2021-11-08 18:20:09 +0000

    Added simple and advanced orders

--- Bot/bot.py
+++ Bot/bot.py
@@ -1,59 +1,70 @@
-import requests
-import json
+import re
 from config import *
+import numpy as np
+import pandas as pd
+import scipy as sc
+import alpaca_trade_api as tradeapi
+from alpaca_trade_api import TimeFrame, TimeFrameUnit
+import logging
 
-# This class communicates between the Alpaca Market API and Okane.
+
+# This class communicates between the Alpaca Trade API and Okane.
+# https://github.com/alpacahq/alpaca-trade-api-python
+
+# Logger to get all messages from different components.
+logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
+
 class Bot:
     def __init__(self):
         # URLs
-        self.ORDERS_BASE_URL = "https://paper-api.alpaca.markets"
-        self.ORDERS_URL = f"{self.ORDERS_BASE_URL}/v2/orders"
+        self.BASE_URL = "https://paper-api.alpaca.markets"
         self.DATA_BASE_URL = "https://data.alpaca.markets"
-        self.ACCOUNT_URL = f"{self.ORDERS_BASE_URL}/v2/account"
 
-        # Headers to authorize access.
-        self.HEADERS = {'APCA-API-KEY-ID': API_KEY, 'APCA-API-SECRET-KEY': SECRET_KEY}
+        # Instantiate REST API
+        self.api = tradeapi.REST(API_KEY, SECRET_KEY, self.BASE_URL, api_version='v2')
 
-    ### "Making Order" Methods ###
-    # Make request to retrieve account.
-    def get_account(self):
-        r = requests.get(self.ACCOUNT_URL, headers=self.HEADERS)
-        return json.loads(r.content)
+    # Simple order w/o stop loss.
+    def simple_order(self, symbol, qty, side, type, time_in_force):
+        self.api.submit_order(
+            symbol=symbol,
+            side=side,
+            type=type,
+            qty=qty,
+            time_in_force=time_in_force
+        )
 
-    # Make Order
-    def create_order(self, symbol, qty, side, type, time_in_force):
-        # Send JSON object
-        data = {
-            "symbol": symbol,
-            "qty": qty,
-            "side": side,
-            "type": type,
-            "time_in_force": time_in_force
-        }
+    # Advanced order w/ stop loss.
+    def advanced_order(self, symbol, qty, side, type, time_in_force, order_class, take_profit, stop_loss):
+        self.api.submit_order(
+            symbol=symbol,
+            side=side,
+            type=type,
+            qty=qty,
+            time_in_force=time_in_force,
+            order_class=order_class,
+            take_profit=take_profit,
+            stop_loss=stop_loss
+        )
 
-        r = requests.post(self.ORDERS_URL, json=data, headers=self.HEADERS)
-        return json.loads(r.content)
-
-        ### "Analysis" Methods ###
-
-    # Get all symbols on the market.
+    # "Analysis" Methods
+    # Get all symbols on the NASDAQ.
     def getAllSymbols(self):
-        self.ASSETS_URL = f"{self.DATA_BASE_URL}/v2/assets"
+        active_assets = self.api.list_assets(status='active')
+        nasdaq_symbols = [a.symbol for a in active_assets if a.exchange == 'NASDAQ']
+        return nasdaq_symbols
 
-    # start, end (str) is required.
-    # limit = no. of data points to return, default 1000, range 1 - 10000.
-    def getHistoricalQuotes(self, symbol, start, end, limit):
-        self.DATA_URL = f"{self.DATA_BASE_URL}/v2/stocks/{symbol}/quotes"
-
-        # Send JSON object
-        data = {
-            "symbol": symbol,
-            "start": start,
-            "end": end,
-            "limit": limit
-        }
-        r = requests.get(self.DATA_URL, json=data, headers=self.HEADERS)
-        return json.loads(r.content)
+    # Returns the bars
+    def getBars(self, symbol, timeframe, start, end):
+        if timeframe == "Minute":
+            return self.api.get_bars(symbol, TimeFrame.Minute, start, end, adjustment='raw').df
+        elif timeframe.__contains__("Minutes"):
+            duration = re.search('(\d+)Minute', timeframe).group(1)
+            return self.api.get_bars(symbol, TimeFrame(int(duration), TimeFrameUnit.Minute), start, end,
+                                     adjustment='raw').df
+        elif timeframe == "Hour":
+            return self.api.get_bars(symbol, TimeFrame.Hour, start, end, adjustment='raw').df
+        elif timeframe == "Day":
+            return self.api.get_bars(symbol, TimeFrame.Day, start, end, adjustment='raw').df
 
     # def analyseSymbol():
     # model = Model()
@@ -64,7 +75,5 @@ class Bot:
 
 # Test Code - Do NOT run this unless you want to execute the order.
 bot = Bot()
-# response = bot.create_order("AAPL", 100, "buy", "market", "gtc")
-# print(response)
-response = bot.getHistoricalQuotes("AAPL", "2010-10-12T07:20:50Z", "2021-11-08T16:49:00Z", 1000)
-print(response)
+print(bot.getBars("AAPL", "Day", "2021-06-08", "2021-06-08"))
+
