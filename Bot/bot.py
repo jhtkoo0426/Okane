1d7cc0531ef3f1a5534a39f38f517e5416cfe44a
jhtkoo0426 <jhtkoo@gmail.com>
2021-11-12 22:44:56 +0000

    Added core database & associated functionalities to make code simpler

--- Bot/bot.py
+++ Bot/bot.py
@@ -7,6 +7,7 @@ import time
 from datetime import datetime
 
 import alpaca_trade_api as tradeapi
+import numpy as np
 import pandas as pd
 import requests
 import websocket
@@ -50,6 +51,9 @@ class Bot:
         self.ws = websocket.WebSocketApp(self.STREAM_URL, on_open=self.on_open, on_message=self.on_message)
         self.ws.run_forever()
 
+        # Dataframes
+        self.core = pd.read_csv('D:/Coding/Projects/Okane/Analysis/core.txt', sep=",")
+
     # 1 | Websocket functions
     # THIS FUNCTION WILL KEEP THE BOT RUNNING. RUN ANY FUNCTIONS THAT REQUIRE
     # LIVE DATA STREAM HERE.
@@ -63,9 +67,7 @@ class Bot:
         }
         self.ws.send(json.dumps(auth_data))
 
-        # symbols = self.getAllSymbols()
-        # symbols = [f"Q.{a}" for a in symbols]
-        # print(symbols[0:10])
+        # Test strategy with popular stocks - Will change this to an analysis-based approach.
         symbols = ['Q.TSLA', 'Q.AAPL', 'Q.MSFT', 'Q.NFLX', 'Q.AMZN']
         listen_message = {"action": "listen", "data": {"streams": symbols}}
         ws.send(json.dumps(listen_message))
@@ -80,10 +82,10 @@ class Bot:
         test = json.loads(message)
         symbol, data = test['stream'][2:], test['data']
         bid, ask = data['p'], data['P']
-        print(symbol, bid, ask)
 
+        self.getSymbolBars(symbol)
         # Implement strategy
-        self.strategy(symbol, bid)
+        self.strategy_moving_av(symbol, bid)
 
     # 2 | Core bot functions
     # Check remaining time till market closes.
@@ -147,7 +149,6 @@ class Bot:
             stop_loss=stop_loss
         )
 
-
     # "Analysis" Methods #
     # Get all symbols on the NASDAQ.
     def getAllSymbols(self):
@@ -164,42 +165,101 @@ class Bot:
                          index_col="Date")
         return df
 
-    # symbol = list of symbols
-    # 100 symbols limit per request!
     # This function gets bars and exports the data to a new .txt file (for every symbol queried).
-    # This function is used to generate the bar charts for bta-lib.
-    def getBar(self, symbol):
+    # This function is used to generate the bar charts for bta-lib for further analysis.
+    def getSymbolBars(self, symbol):
         r = requests.get(f"{self.DATA_BASE_URL}/v1/bars/1D?symbols={symbol}&limit=1000", headers=self.HEADERS)
         data = r.json()
 
         # Check if data file for the symbol exists, if not create file.
         filename = f"D:/Coding/Projects/Okane/Analysis/SymbolsBarsData/{symbol}.txt"
         f = open(filename, 'w+')
-        f.write('Date,Open,High,Low,Close,Volume,OpenInterest\n')
+        f.write('Date,Open,High,Low,Close,Volume\n')
 
         # Get bars of symbol
         bars = data[symbol]
-        # Format each line
 
+        # Format each line
         for bar in bars:
             t = datetime.fromtimestamp(bar['t'])
             day = t.strftime('%Y-%m-%d')
-            line = f"{day},{bar['o']},{bar['h']},{bar['l']},{bar['c']},{bar['v']},0.00\n"
+            line = f"{day},{bar['o']},{bar['h']},{bar['l']},{bar['c']},{bar['v']}\n"
             f.write(line)
 
-    # 5 | Analysis Indicators
+        # Check if symbol in core df.
+        if self.dfSymbolExists(symbol) is False:
+            self.dfAddRow(symbol)
+
+        # Update most recent bars info
+        most_recent_bars = bars[-1]
+        t = datetime.fromtimestamp(most_recent_bars['t'])
+        day = t.strftime('%Y-%m-%d')
+        most_recent_bars['t'] = day
+        self.dfUpdateRow(symbol, most_recent_bars)
+        print(f"[SYSTEM]: Details for {symbol} has been updated.")
+
+    # 5 | Core.txt Dataframe operations (only applicable to the basic info)
+    # date, open, high, low, close, volume.
+    def dfAddColumn(self, colname):
+        if colname not in self.core.columns:
+            self.core[colname] = '0.00'
+        self.saveDf()
+
+    # Add row with symbol as index to core df.
+    def dfAddRow(self, symbol):
+        if symbol not in self.core['Symbol'].tolist():
+            self.core = self.core.append({'Symbol': symbol}, ignore_index=True)
+        self.saveDf()
+
+    # Return the row for a symbol.
+    def dfGetSymbolRow(self, symbol):
+        return self.core.loc[self.core['Symbol'] == symbol]
+
+    # Update a row in core df.
+    def dfUpdateRow(self, symbol, info):
+        # Make sure symbol is already in core df.
+        if self.dfSymbolExists(symbol) is True:
+            # Update
+            self.core.loc[self.core['Symbol'] == symbol, 'Open'] = info['o']
+            self.core.loc[self.core['Symbol'] == symbol, 'High'] = info['h']
+            self.core.loc[self.core['Symbol'] == symbol, 'Low'] = info['l']
+            self.core.loc[self.core['Symbol'] == symbol, 'Close'] = info['c']
+            self.core.loc[self.core['Symbol'] == symbol, 'Volume'] = info['v']
+            self.core.loc[self.core['Symbol'] == symbol, 'LastUpdated'] = info['t']
+        self.saveDf()
+
+    # Check if symbol is present in core df.
+    def dfSymbolExists(self, symbol):
+        return symbol in self.core['Symbol'].tolist()
+
+    def saveDf(self):
+        self.core.to_csv('D:/Coding/Projects/Okane/Analysis/core.txt', sep=',', encoding='utf-8', index=False)
+
+    # 6 | Analysis Indicators
     # A | Simple moving average (SMA)
-    # It calculates the average over a period of time.
-    # Current timeframe is 1Day (1D)
+    # It calculates the average over a period of time. Current timeframe is 1Day (1D)
     def calc_sma(self, symbol, period):
         symbol_df = self.getSymbolDf(symbol)
-        sma = btalib.sma(symbol_df, period=period)  # Returns sma object, 5-day moving av.
+        res = btalib.sma(symbol_df, period=period)  # Returns sma object.
 
-        symbol_df['sma'] = sma.df.fillna(0)  # Fillna(0) replaces NaN values with 0.
+        symbol_df[f"sma{period}"] = res.df.fillna(0)  # Fillna(0) replaces NaN values with 0.
 
-        # Update file
+        # Update individual symbol file
         symbol_df.to_csv(fr'D:/Coding/Projects/Okane/Analysis/SymbolsBarsData/{symbol}.txt', header=True, index=True,
                          sep=",")
+
+        # Add new column for indicator if not in file
+        self.dfAddColumn(f"sma{period}")
+
+        # Update core file
+        last_row = symbol_df.iloc[-1]
+        open, high, low, close, volume, open_interest, sma = last_row[0], last_row[1], last_row[2], last_row[3], last_row[4], last_row[5], last_row[6]
+
+        date = str(symbol_df.index[-1]).split(" ")[0]
+
+        self.core.loc[len(self.core.index)] = [symbol, open, high, low, close, volume, open_interest, date, sma]
+        print(self.core)
+        self.saveDf()
         return symbol_df
 
     # B | Relative Strength Index (RSI)
@@ -220,7 +280,7 @@ class Bot:
         oversold_days = symbol_df[symbol_df['rsi'] < 30]
         # print(oversold_days)
 
-        return symbol_df
+        return symbol_df['sma'][-1]
 
     # C | MOVING-AVERAGE CONVERGENCE/DIVERGENCE (MACD)
     # It measures the distance of a fast and a slow moving average to try to
@@ -243,14 +303,15 @@ class Bot:
 
         return symbol_df
 
-    # 6 | Executing Strategy
-    def strategy(self, symbol, bid):
+    # 7 | Executing Strategy
+    def strategy_moving_av(self, symbol, bid):
         print("Strategy Implemented")
         # https://alpaca.markets/learn/stock-trading-bot-instruction/
         # Get the most current moving average and make comparison.
-        SMA20 = self.calc_sma(symbol, 20)['sma'][-1]  # Gets today's/yesterday's SMA20 from dataframe
-        SMA50 = self.calc_sma(symbol, 50)['sma'][-1]  # Gets today's/yesterday's SMA50 from dataframe
+        SMA20 = self.calc_sma(symbol, 20)  # Gets today's/yesterday's SMA20 from dataframe
+        SMA50 = self.calc_sma(symbol, 50)  # Gets today's/yesterday's SMA50 from dataframe
 
+        print(SMA20, SMA50)
         if SMA20 > SMA50:
             # Buy
             open_pos = self.get_positions(symbol)
@@ -261,47 +322,6 @@ class Bot:
                 # target_position_size = cash_balance / (current_price / self.positionSizing)
                 target_position_size = math.floor(cash*0.1/bid)
 
-                # Authorize order
-                data = {
-                    "symbol": symbol,
-                    "qty": target_position_size,
-                    "side": "buy",
-                    "type": "market",
-                    "time_in_order": "gtc"
-                }
-                r = requests.post(self.ORDER_URL, json=data, headers=self.HEADERS)
-                json.loads(r.content)
-                self.simple_order(symbol, target_position_size, "buy", "market", "gtc")
-                print(f"Placed order:\nSymbol: {symbol} | Action: Buy | Target Position Size: {target_position_size}")
-        else:
-            # Sell
-            open_pos = self.get_positions(symbol)
-            self.simple_order(symbol, open_pos, "sell", "market", "gtc")
-            print(f"Placed Order:\nSymbol: {symbol} | Action: Sell | Sold All")
-
-    def moving_av(self, symbol):
-        # https://alpaca.markets/learn/stock-trading-bot-instruction/
-        # Get the most current moving average and make comparison.
-        SMA20 = self.calc_sma(symbol, 20)['sma'][-1]    # Gets today's/yesterday's SMA20 from dataframe
-        SMA50 = self.calc_sma(symbol, 50)['sma'][-1]    # Gets today's/yesterday's SMA50 from dataframe
-
-        print(f"Calculated SMA:\nSMA20: {SMA20} | SMA50: {SMA50}")
-        # Get owned position for symbol
-        # quantity_owned = self.get_positions(symbol)
-
-        # Execute Strategy
-        if SMA20 > SMA50:
-            # Buy
-            open_pos = self.get_positions(symbol)
-
-            if open_pos == 0:
-                cash_balance = float(self.get_cash_balance())
-
-                # Get last quote of stock (bid price)
-                current_price = float(self.getSymbolLastQuote(symbol)['bidprice'])
-
-                # target_position_size = cash_balance / (current_price / self.positionSizing)
-                target_position_size = math.floor(cash_balance*0.1/current_price)
                 # Authorize order
                 data = {
                     "symbol": symbol,
--- Bot/config.py
+++
@@ -1,3 +0,0 @@
-# Keys needed to authorize account.
-API_KEY = "PK63R5I6ZTH394RWNMAN"
-SECRET_KEY = "2hVF5gzPueaNkpAHl6PLBBOubny4Qbi4O2uIjCMj"
