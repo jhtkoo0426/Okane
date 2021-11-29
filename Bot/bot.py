086c6710dca2ad6fe59443f547de76ba25d5943b
jhtkoo0426 <jhtkoo@gmail.com>
2021-11-29 16:39:48 +0000

    Added constraints to MACD for orders.

--- Bot/bot.py
+++ Bot/bot.py
@@ -109,21 +109,19 @@ class Bot:
         if self.marketIsOpen() is True:
             remaining_time = self.time_to_market_close()
             while remaining_time > 120:
+                # Watch all symbols that are involved:
+                symbols = si.get_day_most_active(25)['Symbol'].to_list()
+                positions = self.getAccountPositions()  # Get all current positions
+                watchlist = list(set(symbols + positions))
+
+
                 # Get bars every 1 minute (with current 15 minute delay).
-                if floor(remaining_time) % 60 == 0:
-                    # Get the current top 10 most traded symbols on the market.
-                    symbols = si.get_day_most_active(20)['Symbol'].to_list()
+                # if floor(remaining_time) % 60 == 0:
+                for symbol in watchlist:
+                    if self.fetchSymbolBars(symbol) is True:        # Prevent empty dataframe error
+                        self.updateBar(symbol)          # Update bar data file
 
-                    # Get all current positions
-                    positions = self.getAccountPositions()
-
-                    # Watch all symbols that are involved:
-                    watchlist = list(set(symbols + positions))
-
-                    for symbol in watchlist:
-                        if self.fetchSymbolBars(symbol) is True:        # Prevent empty dataframe error
-                            self.updateBar(symbol)          # Update bar data file
-                            self.strategy_macd(symbol)      # Calculate the newest MACD from the updated file.
+                        self.strategy_macd(symbol)
                 remaining_time -= 1
                 time.sleep(1)
             if 0 < remaining_time < 120:
@@ -136,7 +134,7 @@ class Bot:
     # Get bars for a symbol. Create a file for the symbol if it doesn't exist. Update the file otherwise.
     def fetchSymbolBars(self, symbol):
         print(f"[SYSTEM]: Fetching most recent bars for {symbol}.")
-        r = requests.get(f"{self.DATA_BASE_URL}/v1/bars/1Min?symbols={symbol}&limit=1000", headers=self.HEADERS)
+        r = requests.get(f"{self.DATA_BASE_URL}/v1/bars/1Min?symbols={symbol}&limit=200", headers=self.HEADERS)
         data = r.json()
         if not data[symbol]:
             print(f"[SYSTEM]: Data collected from {symbol} is insufficient for analysing.")
@@ -219,8 +217,9 @@ class Bot:
 
         print(f"[STRATEGY] Recent data for {symbol}:\nLast Bid: {last_bid_price} | MA: {recent_moving_av} | MACD: {macd} | Signal: {signal} | Histogram: {histogram}")
 
-        if recent_moving_av < last_bid_price < 100:
-            if macd > 0:
+        if recent_moving_av < last_bid_price and macd > 0.1:
+        # if recent_moving_av < last_bid_price < 100:
+            # if macd > 0:
                 # ENTRY: Buy when the MACD crosses over the zero line.
                 # NEW: Only buy if no positions for symbol
                 if self.getPosition(symbol) is None:
@@ -229,7 +228,33 @@ class Bot:
                         print(f"[STRATEGY]: Conditions for order for {symbol} are satisfied. Making order...")
                         self.buyOrder(symbol, quantity)
                         print(f"[STRATEGY]: Bought {quantity} shares of {symbol}.")
-        if macd < 0:
+        try:
+            entry = float(self.getPosition(symbol)['avg_entry_price'])
+            if macd < 0 or (entry is not None and entry * 1.05 > last_bid_price):
+                if self.getPosition(symbol) is not None:
+                    self.strategy_macd_sell(symbol, last_bid_price)
+                else:
+                    pass
+        except TypeError:
+            pass
+
+    def strategy_macd_sell(self, symbol, last_bid_price):
+        # Selling conditions
+        positionDetails = self.getPosition(symbol)
+        print(positionDetails)
+        buyPrice = positionDetails['avg_entry_price']
+        takeProfitPrice = float(buyPrice) * 1.01
+        stopLossPrice = float(buyPrice) * 0.995
+
+        if last_bid_price > takeProfitPrice:
+            print(f"[STRATEGY]: Current price for {symbol} greater than take profit price. Selling for profit :)")
+            # EXIT: Sell at a proft or loss when the MACD crosses below the zero line.
+            quantity = self.getQty(symbol)
+            if quantity is not None:
+                self.sellOrder(symbol, quantity)
+                print(f"[STRATEGY]: Sold all shares of {symbol}.")
+        elif last_bid_price < stopLossPrice:
+            print(f"[STRATEGY]: Current price for {symbol} lower than stop loss price. Selling to minimise loss :(")
             # EXIT: Sell at a proft or loss when the MACD crosses below the zero line.
             quantity = self.getQty(symbol)
             if quantity is not None:
