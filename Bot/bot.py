93caa1feedf0eba798b0199d0ada00fbe3d9d9f7
jhtkoo0426 <jhtkoo@gmail.com>
2022-01-12 19:44:11 +0000

    Solved all dataframe empty bugs; Added profit selling function

--- Bot/bot.py
+++ Bot/bot.py
@@ -13,6 +13,7 @@ from alpaca_trade_api.stream import URL
 from alpaca_trade_api.rest import APIError, TimeFrame
 import yahoo_fin.stock_info as si
 import yfinance as yf
+from requests.exceptions import HTTPError
 
 from Bot.config import API_KEY, SECRET_KEY
 
@@ -81,12 +82,20 @@ class Bot:
         else:
             return position['qty']
 
+    def determineSell(self, symbol, currentPrice):
+        position = self.getPosition(symbol)
+        if position is None:
+            return None
+        else:
+            entry = position['avg_entry_price']
+            if currentPrice > float(entry) * 1.03:
+                # SELL IMMEDIATELY FOR PROFIT
+                return True
+            return False
+
     # Get live (as much as possible) price of symbol
     def getSymbolCurrentPrice(self, symbol):
-        data = yf.download(tickers=symbol, period='1d', interval='1m')
-        last = data.iloc[-1]['Close']
-        print(last)
-        return last
+        return si.get_live_price(symbol)
 
     # Function to determine how many shares to buy
     def determineBuyShares(self, sharesPrice):
@@ -107,21 +116,21 @@ class Bot:
     # Get bars for a symbol.
         # symbol = list of symbols / single symbol
         # hourFrame = 1 hour / 4 hours depending on heiken-ashi strategy
-    # def getHourBars(self, symbol, hourFrame):
-    #     nowTime = datetime.now()
-    #     twoWeekTime = nowTime - timedelta(days=14)
-    #     todayDate = nowTime.strftime("%Y-%m-%d")
-    #     oneWeekDate = twoWeekTime.strftime("%Y-%m-%d")
-    #
-    #     start = pd.Timestamp(oneWeekDate, tz=self.NY).isoformat()
-    #     end = pd.Timestamp(todayDate, tz=self.NY).isoformat()
-    #     bars = self.api.get_bars([symbol], TimeFrame(hourFrame, TimeFrameUnit.Hour), adjustment='raw', start=start, end=end).df
-    #     return bars
+    def getHourBars(self, symbol, hourFrame):
+        try:
+            nowTime = datetime.now()
+            twoWeekTime = nowTime - timedelta(days=14)
+            todayDate = nowTime.strftime("%Y-%m-%d")
+            oneWeekDate = twoWeekTime.strftime("%Y-%m-%d")
 
-    # Using the yfinance API
-    def getHourBars(self, symbol):
-        data = yf.download(tickers=symbol, period='5d', interval='1h')
-        return data
+            start = pd.Timestamp(oneWeekDate, tz=self.NY).isoformat()
+            end = pd.Timestamp(todayDate, tz=self.NY).isoformat()
+            bars = self.api.get_bars([symbol], TimeFrame(hourFrame, TimeFrameUnit.Hour), adjustment='raw', start=start,
+                                     end=end).df
+            return bars
+        except HTTPError:
+            sys.stdout.write(colored(f"[ALPACA SERVER ERROR]: Cannot get data for {symbol}.\n", 'red'))
+            return None
 
     # Technical Indicators
     # Heiken Ashi Candlesticks
@@ -129,28 +138,30 @@ class Bot:
     def calc_ha(self, df):
         dataframe = df.copy()
 
-        # Calculating close bar for HA = 0.25(open + high + low + close)
-        dataframe['HA_close'] = (dataframe['open'] + dataframe['high'] + dataframe['low'] + dataframe['close']) / 4
-        idx = dataframe.index.name
-        dataframe.reset_index(inplace=True)
+        if len(dataframe) != 0:
+            # Calculating close bar for HA = 0.25(open + high + low + close)
+            dataframe['HA_close'] = (dataframe['open'] + dataframe['high'] + dataframe['low'] + dataframe['close']) / 4
+            idx = dataframe.index.name
+            dataframe.reset_index(inplace=True)
 
-        # Calculating open bar for HA = 0.5(ytd open + ytd close)
-        for i in range(0, len(dataframe)):
-            # Determine open bar for HA
-            if i == 0:
-                dataframe._set_value(i, 'HA_open', ((dataframe._get_value(i, 'open') + dataframe._get_value(i, 'close')) / 2))
-            else:
-                dataframe._set_value(i, 'HA_open', ((dataframe._get_value(i - 1, 'HA_open') + dataframe._get_value(i - 1, 'HA_close')) / 2))
+            # Calculating open bar for HA = 0.5(ytd open + ytd close)
+            for i in range(0, len(dataframe)):
+                # Determine open bar for HA
+                if i == 0:
+                    dataframe._set_value(i, 'HA_open', ((dataframe._get_value(i, 'open') + dataframe._get_value(i, 'close')) / 2))
+                else:
+                    dataframe._set_value(i, 'HA_open', ((dataframe._get_value(i - 1, 'HA_open') + dataframe._get_value(i - 1, 'HA_close')) / 2))
 
-        if idx:
-            dataframe.set_index(idx, inplace=True)
+            if idx:
+                dataframe.set_index(idx, inplace=True)
 
-        dataframe['HA_high'] = dataframe[['HA_open', 'HA_close', 'high']].max(axis=1)
-        dataframe['HA_low'] = dataframe[['HA_open', 'HA_close', 'low']].min(axis=1)
+            dataframe['HA_high'] = dataframe[['HA_open', 'HA_close', 'high']].max(axis=1)
+            dataframe['HA_low'] = dataframe[['HA_open', 'HA_close', 'low']].min(axis=1)
 
-        # Add bar type and trend data to symbol dataframe.
-        HADataframe = self.analyseHABars(dataframe)
-        return HADataframe
+            # Add bar type and trend data to symbol dataframe.
+            HADataframe = self.analyseHABars(dataframe)
+            return HADataframe
+        return None
 
     def analyseHABars(self, dataframe):
         df = dataframe.copy()
@@ -250,13 +261,13 @@ class Bot:
 
     # This function determines if a symbol satisfies all the criteria.
     def strategy(self, symbol):
-        one_hour_bars = self.getHourBars(symbol)     # Get 1hr raw dataframe
+        one_hour_bars = self.getHourBars(symbol, 1)     # Get 1hr raw dataframe
 
-        if one_hour_bars.empty:
+        if one_hour_bars is None:
             sys.stdout.write(colored(f"[STRATEGY - {symbol}]: Insufficient data.\n", 'red'))
         else:
             one_hour_ha_bars = self.calc_ha(one_hour_bars)  # Generate HA dataframe (1 hr)
-            if one_hour_ha_bars.empty:
+            if one_hour_ha_bars is None:
                 sys.stdout.write(colored(f"[STRATEGY - {symbol}]: Insufficient data.\n", 'red'))
             else:
                 one_hr_DF = self.calc_ema(one_hour_ha_bars)  # Calculate EMA20 for the dataframe.
@@ -270,6 +281,12 @@ class Bot:
                     trendType = self.determineTrend(one_hr_DF)
                     currentBar, prevBar = one_hr_DF.iloc[-1]['barType'], one_hr_DF.iloc[-2]['barType']
 
+                    sell_bool = self.determineSell(symbol, currentSymbolPrice)
+                    if sell_bool is True:
+                        if self.getPosition(symbol) is not None:
+                            qty = self.getQty(symbol)
+                            self.sellOrder(symbol, qty)
+                            sys.stdout.write(colored(f"[STRATEGY - {symbol} (EMERGENCY SELL)]: Selling all share(s) for profit\n.", 'yellow'))
                     if currentSymbolPrice > currentEMA20:
                         # Buy the symbol - Check for bar conditions (last 2 are BULL, trend is in pullback)
                         self.strategy_buy(symbol, currentBar, trendType, currentSymbolPrice, one_hr_DF)
@@ -278,13 +295,13 @@ class Bot:
                         self.strategy_sell(symbol, currentBar, prevBar, trendType)
 
     def exec(self, remaining_time, watchlist):
-        if floor(remaining_time) % 3600 == 0:
+        # if floor(remaining_time) % 3600 == 0:
             sys.stdout.write(colored("\nRunning Strategy...\n", 'yellow'))
             sys.stdout.write("\033[H\033[J")
             sys.stdout.write(colored(f"[SYSTEM]: Current watchlist has {len(watchlist)} symbols.\n", 'yellow'))
             for index, symbol in enumerate(watchlist):
                 self.strategy(symbol)
-        else:
+        # else:
             sys.stdout.write(f"\rTime to run strategy: {remaining_time % 3600}")
             sys.stdout.flush()
 
@@ -320,22 +337,7 @@ class Bot:
                 print("[SYSTEM]: Market has closed. Bot will now sleep. Goodnight!")
         self.wait_for_market_open()
 
-    # Main Function alternative for testing
-    def mainTesting(self):
-        print("[SYSTEM]: Starting Testing Bot")
-        # Watch all symbols that are involved:
-        symbols = si.get_day_most_active(1)['Symbol'].to_list()
-        positions = self.getAccountPositions()  # Get all current positions
-        # watchlist = list(set(symbols + positions))
-        watchlist = symbols
-        for symbol in watchlist:
-            # self.executeStrategy(symbol)
-            # data = yf.download(tickers=symbol, period='5d', interval='1h')
-            pd.set_option('display.max_columns', None)
-            data = self.getSymbolCurrentPrice(symbol)
-
 
 if __name__ == '__main__':
     bot = Bot()
     bot.start_bot()
-    # bot.mainTesting()
